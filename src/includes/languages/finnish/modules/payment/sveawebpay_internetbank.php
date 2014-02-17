b80a83ede030867e8bf62fb09d0a1c9a9758aa11
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-02-17 10:17:28 +0100

    direct bank fi translations

--- src/includes/languages/finnish/modules/payment/sveawebpay_internetbank.php
+++ src/includes/languages/finnish/modules/payment/sveawebpay_internetbank.php
@@ -1,16 +1,16 @@
 <?php
 /*
-SVEAWEBPAY PAYMENT MODULE FOR osCOMMERCE 2.3
------------------------------------------------
-Version 5.0
-*/
+ * osCommerce 
+ * Svea Internetbank
+ * FI 5.0
+ */
+
 define('MODULE_PAYMENT_SWPINTERNETBANK_TEXT_TITLE','Svea Pankkimaksu');
-define('MODULE_PAYMENT_SWPINTERNETBANK_TEXT_DESCRIPTION','SveaWebPay pankkimaksu Hosted - ver 5.0');
-define('MODULE_PAYMENT_SWPINTERNETBANK_HANDLING_APPLIES','Tilaukseen lisätään %s palvelumaksusta');
-define('ERROR_ALLOWED_CURRENCIES_NOT_DEFINED','Yksi tai useampi sallituista valuutoista ei ole määritelty. Nämä on määriteltävä käyttämään SveaWebPay hosted solutionia. Kirjaudu admin paneeliin ja varmista että.');
+define('MODULE_PAYMENT_SWPINTERNETBANK_TEXT_DESCRIPTION','Svea pankkimaksu - versio 5.0');
+
+define('ERROR_ALLOWED_CURRENCIES_NOT_DEFINED','Yksi tai useampi sallituista valuutoista ei ole määritelty. Nämä on määriteltävä käyttämään Svea Pankkimaksu. Kirjaudu admin paneeliin ja varmista että.');
 define('ERROR_DEFAULT_CURRENCY_NOT_ALLOWED','Standardivaluuta ei ole sallitussa luettelossa. Kirjaudu admin paneeliin ja varmista että oletusvaluutta kuuluu niihin sallituihin maksu-moduulissa.');  
 define('ERROR_MESSAGE_PAYMENT_FAILED','Maksu epäonnistui.');  
-
 define('ERROR_CODE_100','Sisäinen  järjestelmävirhe');
 define('ERROR_CODE_105','Virheellinen tila tapahtuma');
 define('ERROR_CODE_106','Kolmannen puolen epäonnistuminen, esimerkiksi pankissa');
@@ -23,7 +23,6 @@ define('ERROR_CODE_114','Timeout pankissa');
 define('ERROR_CODE_121','Kortin voimassaolo on päätynyt');
 define('ERROR_CODE_124','Hankinnan määrä on ylitetty');
 define('ERROR_CODE_143','Pankki hylkäsi luoton');
-
 
 define('ERROR_CODE_DEFAULT', 'Tuntematon virhe on tapahtunut tilauksen käsittelyssä. Ilmoittakaa tämä koodi kun otatte yhteytä asiakaspalveluun. Virhenumero: ');
 ?>
